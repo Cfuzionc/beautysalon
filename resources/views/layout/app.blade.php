f8fb244adbfd0f4d3e6e6f2bb0d4a75287152b0c
Pasha <57643388+Cfuzionc@users.noreply.github.com>
2022-11-16 14:52:12 +0100

    Beautysalon project

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Meta Tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    @include('layout.partials.assets')
+</head>
+<body data-spy="scroll" data-target=".side_menu" data-offset="90">
+
+
+
+
+<!-- Preloader -->
+    @include('layout.partials.loader')
+<!-- Preloader End -->
+
+<!--Header Start-->
+    @include('layout.partials.navigation')
+<!--Header End-->
+
+<!--Banner Start-->
+<section class="page-title cursor-light">
+    <div class="bg-overlay bg-black opacity-5"></div>
+    <div class="container">
+        <h2 class="hide-cursor">STANDALONE</h2>
+        <ul class="page-breadcrumb link">
+            <!-- <li><a href="../index-barber.html"><span class="icon fas fa-home"></span>Home</a></li> -->
+            <li>StandAlone</li>
+        </ul>
+    </div>
+</section>
+<!--Banner End-->
+
+<!--main page content-->
+    <main id="app">
+        @yield('content')
+    </main>
+        <div class="container">
+            <div class="row no-gutters">
+                <div class="col-12">
+                    <!-- START HEADING SECTION -->
+                    <div class="standalone-detail">
+                        <div class="row no-gutters">
+                            <div class="col-12 col-md-10 offset-md-1 col-lg-8 offset-lg-2  text-center wow slideInUp" data-wow-duration="2s">
+                                <p class="sub-heading text-center">Most flexible one page</p>
+                                <h1 class="heading">SECTION TITLE</h1>
+                                <p class="para_text m-auto">Curabitur mollis bibendum luctus. Duis suscipit vitae dui sed suscipit. Vestibulum auctor nunc vitae diam eleifend, in maximus metus sollicitudin. Quisque vitae sodales lectus. Nam porttitor justo sed mi finibus, vel tristique risus faucibus.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <!--content-->
+                    <div class="standalone-area">
+                        <div class="row standalone-row align-items-center no-gutters">
+                            <div class="col-lg-6">
+                                <div class="blog-image wow hover-effect fadeInLeft">
+                                    <img src="{{ asset('img/stand1.jpg') }}" alt="image">
+                                </div>
+                            </div>
+                            <div class="col-lg-6 stand-img-des">
+                                <div class="d-inline-block">
+                                    <p class="sub-heading text-center">Most flexible one page</p>
+                                    <h2 class="gradient-text1">STANDALONE HEADING</h2>
+                                    <p class="para_text">Curabitur mollis bibendum luctus. Duis suscipit vitae dui sed suscipit. Vestibulum auctor nunc vitae diam eleifend, in maximus metus sollicitudin. </p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!--content-->
+                        <div class="row standalone-row align-items-center no-gutters">
+                            <div class="col-lg-6 order-lg-2">
+                                <div class="blog-image wow hover-effect fadeInRight">
+                                    <img src="{{ asset('img/stand2.jpg') }}" alt="image">
+                                </div>
+                            </div>
+                            <div class="col-lg-6 stand-img-des">
+                                <div class="d-inline-block">
+                                    <p class="sub-heading text-center">Most flexible one page</p>
+                                    <h2 class="gradient-text1">STANDALONE HEADING</h2>
+                                    <p class="para_text">Curabitur mollis bibendum luctus. Duis suscipit vitae dui sed suscipit. Vestibulum auctor nunc vitae diam eleifend, in maximus metus sollicitudin. </p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!--content-->
+                        <div class="row standalone-row align-items-center no-gutters">
+                            <div class="col-lg-6">
+                                <div class="blog-image wow hover-effect fadeInLeft">
+                                    <img src="{{ asset('img/stand3.jpg') }}" alt="image">
+                                </div>
+                            </div>
+                            <div class="col-lg-6 stand-img-des">
+                                <div class="d-inline-block">
+                                    <p class="sub-heading text-center">Most flexible one page</p>
+                                    <h2 class="gradient-text1">STANDALONE HEADING</h2>
+                                    <p class="para_text">Curabitur mollis bibendum luctus. Duis suscipit vitae dui sed suscipit. Vestibulum auctor nunc vitae diam eleifend, in maximus metus sollicitudin. </p>
+                                </div>
+                            </div>
+                        </div>
+                        <!--content-->
+                    </div>
+                    <!-- END HEADING SECTION -->
+                </div>
+            </div>
+        </div>
+<!--main page content end-->
+
+<!--Footer Start-->
+    @include('layout.partials.footer')
+<!--Footer End-->
+
+<!--Scroll Top Start-->
+<span class="scroll-top-arrow"><i class="fas fa-angle-up"></i></span>
+<!--Scroll Top End-->
+</body>
+</html>
---
+++ resources/views/layout/partials/navigation.blade.php
@@ -0,0 +1,66 @@
+<header>
+
+    <!--Navigation-->
+    <nav class="navbar navbar-top-default navbar-expand-lg navbar-simple nav-line">
+        <div class="container">
+            <a href="../index-barber.html" title="Logo" class="logo scroll">
+                <!--Logo Default-->
+                <img src="{{ asset('img/Logo2.png') }}" alt="logo" class="logo-white">
+                <img src="{{ asset('img/logo2.png') }}" alt="logo" class="logo-dark">
+            </a>
+
+            <!--Nav Links-->
+            <div class="collapse navbar-collapse" id="Test">
+                <div class="navbar-nav ml-auto">
+                    <a class="nav-link active" href="/">Home</a>
+                    <a class="nav-link" href="/">About</a>
+                    <a class="nav-link" href="/">Reservate</a>
+                    <a class="nav-link" href="/">Contact</a>
+                </div>
+            </div>
+
+            <!--Side Menu Button-->
+            <a href="javascript:void(0)" class="sidemenu_btn" id="sidemenu_toggle">
+                <span></span>
+                <span></span>
+                <span></span>
+            </a>
+
+        </div>
+    </nav>
+
+    <!--Side Nav-->
+    <div class="side-menu hidden">
+        <div class="inner-wrapper">
+            <span class="btn-close" id="btn_sideNavClose"><i></i><i></i></span>
+            <nav class="side-nav w-100">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#rev-slider">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="">Reserve now</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="">Contact</a>
+                    </li>
+                </ul>
+            </nav>
+
+            <div class="side-footer w-100">
+                <ul class="social-icons-simple">
+                    <li><a class="facebook-text-hvr" href="javascript:void(0)"><i class="fab fa-facebook-f"></i> </a> </li>
+                    <li><a class="instagram-text-hvr" href="javascript:void(0)"><i class="fab fa-instagram"></i> </a> </li>
+                    <li><a class="twitter-text-hvr" href="javascript:void(0)"><i class="fab fa-twitter"></i> </a> </li>
+                </ul>
+                <p>&copy; Test</p>
+            </div>
+        </div>
+    </div>
+    <a id="close_side_menu" href="javascript:void(0);"></a>
+    <!-- End side menu -->
+
+</header>
